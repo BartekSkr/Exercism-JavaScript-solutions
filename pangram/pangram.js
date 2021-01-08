3124bab51146016681e6089878ccf57f7fea672d
BartekSkr <bartoszskorka86@gmail.com>
2021-01-08 12:27:21 +0100

    Pangram

---
+++ pangram/pangram.js
@@ -0,0 +1,37 @@
+const ALPHABET = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+export const isPangram = sentence => {
+  return ALPHABET.every(letter =>
+    sentence
+      .toLowerCase()
+      .split("")
+      .includes(letter)
+  );
+};
